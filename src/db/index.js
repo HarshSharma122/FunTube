45e48ed158563d6887d4da718c08ef2556ce548c
DsaBegin <harsh444577@gmail.com>
2025-03-28 14:18:02 +0530

    database connection is completed

---
+++ src/db/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    const connectInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/ ${DB_NAME}`
+    );
+
+    console.log(
+      `\n mongodb is connected || DB HOST:${connectInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("Connection error while fetching the data with mongodb", error);
+    process.exit(1);
+  }
+};
+export default connectDB;
