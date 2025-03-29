5d09b28d12b7853312179eb00b54f1f84c45099d
DsaBegin <harsh444577@gmail.com>
2025-03-29 09:22:32 +0530

    create some util class

--- src/index.js
+++ src/index.js
@@ -1,35 +1,24 @@
 // require('dotenv').config({path: './env'})
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path:'./env'
-})
+  path: "./env",
+});
 
 connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Database connection failed", error);
+  });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  // npm install cookie-parser
 
 /*
 import express from "express";
